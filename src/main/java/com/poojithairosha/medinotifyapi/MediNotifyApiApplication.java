a2530b50d5b2b4ab4e4967ca4f8bf90a4fc774df
Poojitha Irosha <poojithairosha9311@gmail.com>
2026-04-28 01:26:16 +0530

    initial commit

---
+++ src/main/java/com/poojithairosha/medinotifyapi/MediNotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.poojithairosha.medinotifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediNotifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediNotifyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poojithairosha/medinotifyapi/MediNotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poojithairosha.medinotifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediNotifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
